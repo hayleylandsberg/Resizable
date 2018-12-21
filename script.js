74718b0dcae4009ea76c839757a3388f759812b3
Hayley Landsberg <hayleyheath@gmail.com>
2018-12-21 14:13:47 -0600

    made divs resizabl with jquery offset

---
+++ script.js
@@ -0,0 +1,28 @@
+let isResizing = false,
+    lastDownX = 0;
+
+$(function () {
+    let container = $('#container'),
+        left = $('#left'),
+        right = $('#right'),
+        handle = $('#handle');
+
+    handle.on('mousedown', function (e) {
+        isResizing = true;
+        lastDownX = e.clientX;
+    });
+
+    $(document).on('mousemove', function (e) {
+        // we don't want to do anything if we aren't resizing.
+        if (!isResizing) 
+            return;
+
+        let offsetRight = container.width() - (e.clientX - container.offset().left);
+
+        left.css('right', offsetRight);
+        right.css('width', offsetRight);
+    }).on('mouseup', function (e) {
+        // stop resizing
+        isResizing = false;
+    });
+});
